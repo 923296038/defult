80a82e1738458a0b7e602379f17392eba904c45c
923296038 <923296038@qq.com>
2019-07-25 19:16:32 +0800

    ssm实现查询数据

---
+++ paperSpringMVC/src/main/java/com/timeUtil/DateTag.java
@@ -0,0 +1,28 @@
+package com.timeUtil;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateTag extends TagSupport {
+    private String value;
+    @Override
+    public int doStartTag() throws JspException {
+        String vv = "" + value;
+        try {
+            long time = Long.valueOf(vv.trim());
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(time * 1000);
+            SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+            String s = dateformat.format(c.getTime());
+            pageContext.getOut().write(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return super.doStartTag();
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
